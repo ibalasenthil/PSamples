95a3b1a4a8fe6d71570dba9ee8cd6e5effbaebed
ibala <balaxyz@ymail.com>
2020-02-19 03:02:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace allVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte number = 2;
+            //int count = 10;
+            //float totalPrice = 20.95f;
+            //char character = 'A';
+            //string firstName = "Bala";
+            //bool isworking = false;
+
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Bala";
+            var isworking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isworking);
+
+
+
+
+          
+
+        }
+    }
+}
